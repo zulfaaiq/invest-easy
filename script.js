eefb3ffc44c74589c2d95f5255cde5e73d1acdc3
Faiqotuzzulfa <faiqotuzzulfa05@students.unnes.ac.id>
2023-01-14 09:49:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const review = document.getElementsByClassName('testi');
+review.innerHTML = 'Opening a stock account at Ajaib Sekuritas is the easiest compared to other brokers because registration at Invest Easy is completely online';
+
